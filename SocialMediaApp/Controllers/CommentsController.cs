eeff936c87e0c3631a833d9b075c1658ea4f989f
ROXANA-GEORGIANAASAV <asaveiroxana7@gmail.com>
2024-12-07 15:13:08 +0200

    de rezolvat posts/show

--- SocialMediaApp/Controllers/CommentsController.cs
+++ SocialMediaApp/Controllers/CommentsController.cs
@@ -7,22 +7,47 @@ namespace SocialMediaApp.Controllers
 {
 	public class CommentsController : Controller
 	{
-        private readonly ApplicationDbContext db;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        public CommentsController(
-        ApplicationDbContext context,
-        UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager
-        )
-        {
-            db = context;
-            _userManager = userManager;
-            _roleManager = roleManager;
-        }
-        public IActionResult Index()
+		private readonly ApplicationDbContext db;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+		public CommentsController(
+		ApplicationDbContext context,
+		UserManager<ApplicationUser> userManager,
+		RoleManager<IdentityRole> roleManager
+		)
 		{
-			return View();
+			db = context;
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+		[HttpPost]
+		public IActionResult Delete(int id)
+		{
+			Comment comm = db.Comments.Find(id);
+			db.Comments.Remove(comm);
+			db.SaveChanges();
+			return Redirect("/Posts/Show/" + comm.PostId);
+		}
+		public IActionResult Edit(int id)
+		{
+			Comment comm = db.Comments.Find(id);
+			return View(comm);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(int id, Comment requestComment)
+		{
+			Comment comm = db.Comments.Find(id);
+			try
+			{
+				comm.Continut = requestComment.Continut;
+				db.SaveChanges();
+				return Redirect("/Posts/Show/" + comm.PostId);
+			}
+			catch (Exception)
+			{
+				return View(requestComment);
+			}
 		}
 	}
 }
--- SocialMediaApp/Models/Comment.cs
+++ SocialMediaApp/Models/Comment.cs
@@ -6,12 +6,10 @@ namespace SocialMediaApp.Models
 	{
 		[Key]
 		public int Id { get; set; }
-
-		public string Continut{ get; set; }
+		public string? Continut { get; set; }
 		public int? PostId { get; set; }
 		public string? UserId { get; set; }
 		public DateTime Data { get; set; }
-
 		public virtual Post Post { get; set; }
 		public virtual ApplicationUser User { get; set; }
 
