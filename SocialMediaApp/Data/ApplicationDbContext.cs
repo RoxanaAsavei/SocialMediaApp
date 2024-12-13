96e5ba753c7de266c3a83c3137f75cb8b3f53ecc
Theo <ancutatheodor17@gmail.com>
2024-12-13 17:58:27 +0200

    si group

--- SocialMediaApp/Data/ApplicationDbContext.cs
+++ SocialMediaApp/Data/ApplicationDbContext.cs
@@ -4,58 +4,63 @@ using SocialMediaApp.Models;
 
 namespace SocialMediaApp.Data
 {
-	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-		}
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-		public DbSet<Comment> Comments { get; set; }
-		public DbSet<Post> Posts { get; set; }
-		public DbSet<Tag> Tags { get; set; }
-		public DbSet<Group> Groups { get; set; }
-		public DbSet<UserGroup> UserGroups { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-			// Configure cascade delete for Comment and Post
-			modelBuilder.Entity<Comment>()
-				.HasOne(c => c.Post)
-				.WithMany(p => p.Comments)
-				.HasForeignKey(c => c.PostId)
-				.OnDelete(DeleteBehavior.Cascade);
+            // Configure cascade delete for Comment and Post
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			// Configure cascade delete for Tag and Post
-			modelBuilder.Entity<Post>()
-				.HasOne(p => p.Tag)
-				.WithMany(t => t.Posts)
-				.HasForeignKey(p => p.TagId)
-				.OnDelete(DeleteBehavior.Cascade);
+            // Configure cascade delete for Tag and Post
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Tag)
+                .WithMany(t => t.Posts)
+                .HasForeignKey(p => p.TagId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			// Configure foreign key for Post and User with NO ACTION
-			modelBuilder.Entity<Post>()
-				.HasOne(p => p.User)
-				.WithMany(u => u.Posts)
-				.HasForeignKey(p => p.UserId)
-				.OnDelete(DeleteBehavior.NoAction);
+            // Configure foreign key for Post and User with NO ACTION
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
 
-			// Configure foreign key for Tag and User with NO ACTION
-			modelBuilder.Entity<Tag>()
-				.HasOne(t => t.User)
-				.WithMany(u => u.Tags)
-				.HasForeignKey(t => t.UserId)
-				.OnDelete(DeleteBehavior.NoAction);
+            // Configure foreign key for Tag and User with NO ACTION
+            modelBuilder.Entity<Tag>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tags)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
 
-			// Configure foreign key for Comment and User with NO ACTION
-			modelBuilder.Entity<Comment>()
-				.HasOne(c => c.User)
-				.WithMany(u => u.Comments)
-				.HasForeignKey(c => c.UserId)
-				.OnDelete(DeleteBehavior.NoAction);
-		}
-	}
+            // Configure foreign key for Comment and User with NO ACTION
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Group)
+                .WithMany(g => g.Posts)
+                .HasForeignKey(p => p.GroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
 }
-
---
+++ SocialMediaApp/Data/Migrations/20241213145520_GroupfaraEdit.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupfaraEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Groups_GroupId",
+                table: "Posts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Groups_GroupId",
+                table: "Posts",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Groups_GroupId",
+                table: "Posts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Groups_GroupId",
+                table: "Posts",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
